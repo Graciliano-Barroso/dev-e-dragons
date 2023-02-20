d174b8105ead0520237740590919b8079f81f64e
Graciliano-Barroso <grico484@gmail.com>
2023-02-20 06:26:58 -0400

    feat: Import e export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}
+import { Personagem } from "./modules/personagem.js"
+
+// const personagemPedrinho = {
+//     nome: 'Pedrinho',
+//     vida: 7,
+//     mana: 12,
+//     level: 5,
+//     tipo: 'Mago',
+// }
 
 const personagemJose = {
     nome: 'Jose',
@@ -14,3 +16,9 @@ const personagemJose = {
     tipo: 'Arqueiro',
 }
 
+const personagemPedrinho = new Personagem()
+personagemPedrinho.nome = "Pedrinho"
+personagemPedrinho.mana = 12
+personagemPedrinho.vida = 7
+
+console.log(personagemPedrinho)
