9447a96c9a1b3a379a3e35c6c8d9c6189ccae710
Graciliano-Barroso <grico484@gmail.com>
2023-02-24 20:03:13 -0400

    feat: Criação do método obterInsignia

--- src/index.js
+++ src/index.js
@@ -8,17 +8,25 @@ import { Personagem } from "./modules/personagem.js"
 //     tipo: 'Mago',
 // }
 
-const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}
+// const personagemJose = {
+//     nome: 'Jose',
+//     vida: 7,
+//     mana: 6,
+//     level: 3,
+//     tipo: 'Arqueiro',
+// }
 
 const personagemPedrinho = new Personagem()
 personagemPedrinho.nome = "Pedrinho"
 personagemPedrinho.mana = 12
 personagemPedrinho.vida = 7
+personagemPedrinho.tipo = "Mago"
+personagemPedrinho.level = 5
 
-console.log(personagemPedrinho)
+const personagemJose = new Personagem()
+personagemJose.nome = "Jose"
+personagemJose.tipo = "Arqueiro"
+personagemJose.level = 3
+
+console.log('Insignia de ' + personagemPedrinho.nome + ': ' + personagemPedrinho.obterInsignia())
+console.log('Insignia de ' + personagemJose.nome + ': ' + personagemJose.obterInsignia())
