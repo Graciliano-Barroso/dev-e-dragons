d10d513895f5286ddf6df99071846db7bf75eefa
Graciliano-Barroso <grico484@gmail.com>
2023-03-03 17:33:15 -0400

    feat: Renderizando personagem na página

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import { PersonagemView } from "./components/personagem-view.js"
 import { Personagem } from "./modules/personagem.js"
 
 const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago')
 const personagemJose = new Personagem('José', 3, 'Arqueiro')
+
+const personagem = [personagemPedrinho, personagemJose]
+
+new PersonagemView(personagem).render()
