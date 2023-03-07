17ffe6236b5d2e5a8ca4d9e21452846302f6ba14
Graciliano-Barroso <grico484@gmail.com>
2023-03-07 06:07:11 -0400

    Sobrescrevendo métodos

--- src/modules/mago.js
+++ src/modules/mago.js
@@ -11,7 +11,14 @@ export class Mago extends Personagem {
         this.elementoMagico = elementoMagico
         this.levelMagico = levelMagico
         this.inteligencia = inteligencia
-    }    
+    }
+
+    obterInsignia() {
+        if (this.levelMagico >= 5 && this.inteligencia >= 5) {
+            return `Mestre do ${this.elementoMagico}`
+        }
+        return super.obterInsignia()
+    }
 }
 
 console.log(new Mago('gelo', 7, 9))
